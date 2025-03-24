66f9b3f3db209e5980c0c0d7e0a82259d9597ac7
ClemensU42 <clemens_u@web.de>
2025-03-24 15:39:37 +0100

    added crates and created window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use winit::{event::{Event, WindowEvent}, event_loop::EventLoop, window::WindowBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, control_flow| match event{
+        Event::WindowEvent { 
+            window_id, 
+            ref event 
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested => control_flow.exit(),
+            _ => {}
+        },
+        _ => {}
+    }).unwrap();
 }
