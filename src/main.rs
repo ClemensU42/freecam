6c80bfbdebf84bea70e86027a3ec5315bb1321a1
ClemensU42 <clemens_u@web.de>
2025-03-24 21:52:21 +0100

    added proper window creation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
+use window::state::State;
 use winit::{event::{Event, WindowEvent}, event_loop::EventLoop, window::WindowBuilder};
 
-fn main() {
+mod window;
+
+fn main(){
+    pollster::block_on(run());
+}
+
+async fn run() {
     env_logger::init();
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = State::new(&window).await;
 
     event_loop.run(move |event, control_flow| match event{
         Event::WindowEvent { 
             window_id, 
             ref event 
-        } if window_id == window.id() => match event {
+        } if window_id == state.window().id() => match event {
             WindowEvent::CloseRequested => control_flow.exit(),
             _ => {}
         },
